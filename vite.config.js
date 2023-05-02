2c31a021cc1e2fbab54ce8eaf88ef289c576c068
matiasAndrada <matiasandrada5622@gmail.com>
2023-05-02 14:17:01 -0300

    pc

--- vite.config.js
+++ vite.config.js
@@ -4,34 +4,12 @@ import reactRefresh from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
-    sourcemap: true,
-    assetsInlineLimit: 0,
     rollupOptions: {
-      output: {
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[hash].[ext]',
-        format: 'es',
-        manualChunks: undefined,
-        preserveModules: true,
-        preferConst: true,
-        sourcemap: true,
-      },
+      input: 'src/main.jsx',
     },
+    outDir: 'dist',
+    assetsDir: 'public',
+    assetsInclude: 'src/assets/**',
+    root: './public',
   },
-  server: {
-    host: '0.0.0.0',
-    fs: {
-      strict: false,
-    },
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-    open: true,
-    port: 3000,
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/public/'
-    : '/',
 });

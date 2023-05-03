618852a9a314a312371bf8a4524d0ca927123d5e
matiasAndrada <matiasandrada5622@gmail.com>
2023-05-02 23:59:35 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,9 @@ import ReactDOM from "react-dom";
 import "./styles/main.scss";
 import App from "./App";
 
-console.log("Starting to render");
-
 const rootElement = document.getElementById("root");
 if (!rootElement) {
-  console.error("Unable to find #root element");
   throw new Error("Unable to find #root element");
 }
 
-ReactDOM.render(<App />, rootElement);
-
-console.log("Rendering done");
+ReactDOM.createRoot(rootElement).render(<App />);

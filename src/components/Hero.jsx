959be58f076d7bf74d9760dd5860a3111ba919e3
matiasAndrada <matiasandrada5622@gmail.com>
2023-05-05 19:15:42 -0300

    deploy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
     
         <div className="fullpage__header-left">
           <div>
-            <h1 className="fullpage__header__name ">Matías Andrada.</h1>
+            <h1 className="fullpage__header__name ">.</h1>
             <h3 className="fullpage__header__role" >
               Desarrollador Full Stack Junior.
             </h3>

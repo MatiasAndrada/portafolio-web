14295d029158efd3c2bc75181efa356f45f05bbc
matiasAndrada <matiasandrada5622@gmail.com>
2023-05-08 11:17:58 -0300

    build

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,33 +1,37 @@
-import React, { useMemo, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+import React, { Suspense, useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import PoissonDiskSampling from 'poisson-disk-sampling';
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = useMemo(() => {
-    const positions = new Float32Array(1000 * 3); // Usamos una matriz Typed de 1000 puntos en lugar de crear una matriz en cada renderizado
-    random.inSphere(positions, { radius: 0.8 }); // Llenamos la matriz con las posiciones aleatorias
+
+  // Crea una instancia de PoissonDiskSampling con los parámetros deseados
+  const pds = new PoissonDiskSampling({
+    shape: [1, 1, 1], // Tamaño del espacio en el que se generan los puntos
+    minDistance: 0.10, // Distancia mínima entre los puntos generados
+    maxDistance: 0.20, // Distancia máxima entre los puntos generados
+    tries: 20, // Número máximo de intentos para generar un nuevo punto
+  });
+
+  // Genera los puntos utilizando PoissonDiskSampling
+  const [points, setPoints] = useState(() => {
+    const positions = new Float32Array(pds.fill().map((p) => [p[0] - 0.5, p[1] - 0.5, p[2] - 0.5]).flat());
     return positions;
-  }, []);
+  });
 
   useFrame((state, delta) => {
-    // Memoizamos la función useFrame para evitar que se vuelva a ejecutar en cada renderizado
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
 
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points
-        ref={ref}
-        positions={sphere}
-        frustumCulled={false} // Desactivamos la culling de frustum para que los puntos se dibujen fuera de la cámara
-        {...props}
-      >
-        <pointsMaterial
+    <group>
+      <Points ref={ref} positions={points} {...props}>
+        <PointMaterial
           transparent
-          size={0.002} // Aumentamos el tamaño de los puntos
+          color='#f272c8'
+          size={0.003}
           sizeAttenuation={true}
           depthWrite={false}
         />
@@ -40,7 +44,10 @@ const StarsCanvas = () => {
   return (
     <div className="absolute inset-0 z-[10]">
       <Canvas camera={{ position: [0, 0, 1] }}>
-        <Stars />
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
         <Preload all />
       </Canvas>
     </div>

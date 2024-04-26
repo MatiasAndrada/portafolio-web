0690804394dfac982c0b04295efaed581757b35d
Matias Andrada <matiasandrada5622@gmail.com>
2024-04-26 16:22:35 -0300

    Actualiza enlaces y estilos de la página principal y los iconos de los proyectos

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 import { styles } from '../styles'
@@ -16,12 +16,20 @@ const ProjectCard = ({
   source_code_link,
   source_demo_link
 }) => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    // Detecta si se trata de un dispositivo móvil por el ancho de pantalla
+    const screenWidth = window.innerWidth
+    setIsMobile(screenWidth <= 500) // Cambia el valor según tus necesidades
+  }, [])
+
   const optionsTilt = {
     reverse: false, // reverse the tilt direction
-    max: 30, // max tilt rotation (degrees)
-    perspective: 1300, // Transform perspective, the lower the more extreme the tilt gets.
-    scale: 1.01, // 2 = 200%, 1.5 = 150%, etc..
-    speed: 1000, // Speed of the enter/exit transition
+    max: isMobile ? 7 : 30, // max tilt rotation (degrees)
+    perspective: isMobile ? 1800 : 1300, // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+    speed: isMobile ? 400 : 1000, // Speed of the enter/exit transition
     transition: true, // Set a transition on enter/exit.
     axis: null, // What axis should be disabled. Can be X or Y.
     reset: true, // If the tilt effect has to be reset on exit.
